f7fb6b7e9a0e946bdc0351e015547bc7709723fe
Jhacy456 <jakpesad20@gmail.com>
2024-10-18 16:57:54 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='hero'>
+    <h1>FreshPick</h1>
+    <p>Fresh Fruits and vegetables right at your doorstep!</p>
+    
+    </div>
+  );
+}
+
+export default App
